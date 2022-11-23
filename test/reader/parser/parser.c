ee8edc6cac325184cd7c603802c5309a76d8613a
jcmdln <jcmdln@gmail.com>
2022-11-23 16:38:24 -0500

    test: reader: parser: Add scaffolding

--- test/reader/parser/parser.c
+++ test/reader/parser/parser.c
@@ -2,12 +2,39 @@
 //
 // Copyright (c) 2022 Johnathan C Maudlin <jcmdln@gmail.com>
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+#include <ploy/builtins.h>
 #include <ploy/reader/parser.h>
 
 int
 main(void)
 {
+	puts("form: (wew :lad \"foo bar\")");
+	Print(Read("wew :lad \"foo bar\""));
+	Print(Read("(wew :lad \"foo bar\")"));
+	putchar('\n');
+
+	puts("form: (wew :lad \"foo bar\" (wew :lad \"foo bar\"))");
+	Print(Read("wew :lad \"foo bar\" (wew :lad \"foo bar\")"));
+	Print(Read("(wew :lad \"foo bar\" (wew :lad \"foo bar\"))"));
+	putchar('\n');
+
+	puts("form: (wew :lad \"foo bar\" (wew :lad \"foo bar\" (wew :lad \"foo bar\")))");
+	Print(Read("wew :lad \"foo bar\" (wew :lad \"foo bar\" (wew :lad \"foo bar\"))"));
+	Print(Read("(wew :lad \"foo bar\" (wew :lad \"foo bar\" (wew :lad \"foo bar\")))"));
+	putchar('\n');
+
+	puts("form: (((wew :lad \"foo bar\") wew :lad \"foo bar\") wew :lad \"foo bar\")");
+	Print(Read("((wew :lad \"foo bar\") wew :lad \"foo bar\") wew :lad \"foo bar\""));
+	Print(Read("(((wew :lad \"foo bar\") wew :lad \"foo bar\") wew :lad \"foo bar\")"));
+	putchar('\n');
+
+	puts("form: ((wew :lad \"foo bar\") wew :lad \"foo bar\")");
+	Print(Read("(wew :lad \"foo bar\") wew :lad \"foo bar\""));
+	Print(Read("((wew :lad \"foo bar\") wew :lad \"foo bar\")"));
+
 	return EXIT_SUCCESS;
 }
