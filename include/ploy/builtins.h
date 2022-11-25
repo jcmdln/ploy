5192fea4abbf2534669f61cecf61efe9cc2b9c8d
jcmdln <jcmdln@gmail.com>
2022-11-25 11:42:10 -0500

    builtins: Uncomment the next things to implement

--- include/ploy/builtins.h
+++ include/ploy/builtins.h
@@ -11,15 +11,15 @@ struct object *Append(struct object *list, struct object *object);
 struct object *Car(struct object *list);
 struct object *Cdr(struct object *list);
 struct object *Cons(struct object *car, struct object *cdr);
-// struct object *Define(struct object *env, struct object *symbol, struct object *value);
+struct object *Define(struct object *env, struct object *symbol, struct object *value);
 struct object *Eval(struct object *object);
-// struct object *For(struct object *expr, struct object *body);
-// struct object *If(struct object *expr, struct object *body);
-// struct object *Lambda(struct object *env, struct object *args, struct object *body);
+struct object *For(struct object *expr, struct object *body);
+struct object *If(struct object *expr, struct object *body);
+struct object *Lambda(struct object *env, struct object *args, struct object *body);
 struct object *Print(struct object *object);
+struct object *Quasiquote(struct object *object);
+struct object *Quote(struct object *object);
 struct object *Read(const char *input);
-// struct object *Quasiquote(struct object *object);
-// struct object *Quote(struct object *object);
 struct object *Reverse(struct object *object);
 
 #endif // PLOY_BUILTINS_H
