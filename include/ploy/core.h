859264964a7e80b53c71fd379852d266b9c0f501
jcmdln <jcmdln@gmail.com>
2023-09-30 23:12:13 -0400

    Add note to implement Format()

--- include/ploy/core.h
+++ include/ploy/core.h
@@ -16,6 +16,7 @@ Object const *Cons(Object const *car, Object const *cdr);
 // Object const *Define(Object const *env, Object const *symbol, Object const *value);
 Object const *Eval(Object const *object);
 // Object const *For(Object const *expr, Object const *body);
+// Object const *Format(char const *form, ...);
 // Object const *If(Object const *expr, Object const *body);
 // Object const *Lambda(Object const *env, Object const *args, Object const *body);
 // Object const *Let(Object const *env, Object const *args, Object const *body);
